372f6f72ed8488d2a2150f2d58c16dd80a9f0a64
apipko <anatolii.pipko@eqs.com>
2023-12-06 10:04:02 +0000

    add tabs to lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -8,14 +8,24 @@ return {
   },
   config = function()
     require('lualine').setup {
-      -- Show icon in there are unsaved buffers
       sections = {
+        lualine_a = {
+          {
+            'buffers',
+            symbols = {
+              modified = ' 💾', -- Text to show when the buffer is modified
+
+              alternate_file = '#', -- Text to show to identify the alternate file
+              directory = '', -- Text to show when the buffer is a directory
+            },
+          }
+        },
         lualine_z = {
           {
             function()
               for _, buf in ipairs(vim.api.nvim_list_bufs()) do
                 if vim.api.nvim_buf_get_option(buf, 'modified') then
-                  return '💾'           -- any message or icon
+                  return '💾' -- any message or icon
                 end
               end
               return ''
