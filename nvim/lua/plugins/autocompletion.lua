e74cc93fcaa5467845aac1e829230310ae63b163
apipko <anatolii.pipko@eqs.com>
2023-12-14 19:49:49 +0000

    add lspkind nvim plugin

--- nvim/lua/plugins/autocompletion.lua
+++ nvim/lua/plugins/autocompletion.lua
@@ -1,10 +1,10 @@
 return {
   'hrsh7th/nvim-cmp',
-  event = "InsertEnter",
+  event = 'InsertEnter',
   dependencies = {
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
     -- Snippet Engine & its associated nvim-cmp source
     'L3MON4D3/LuaSnip',
     {
@@ -18,6 +18,9 @@ return {
 
     -- Adds a number of user-friendly snippets
     'rafamadriz/friendly-snippets',
+
+    -- icons for autocomplete
+    'onsails/lspkind.nvim',
   },
   config = function()
     -- [[ configure nvim-cmp ]]
@@ -27,10 +30,11 @@ return {
     -- See `:help cmp`
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
+    local lspkind = require 'lspkind'
     require('luasnip.loaders.from_vscode').lazy_load()
     luasnip.config.setup {}
 
-    cmp.setup({
+    cmp.setup {
       snippet = {
         -- REQUIRED - you must specify a snippet engine
         expand = function(args)
@@ -44,23 +48,25 @@ return {
         -- completion = cmp.config.window.bordered(),
         -- documentation = cmp.config.window.bordered(),
       },
-      mapping = cmp.mapping.preset.insert({
+      mapping = cmp.mapping.preset.insert {
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      }),
-      sources = cmp.config.sources({
+        ['<CR>'] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      },
+      sources = cmp.config.sources {
         { name = 'nvim_lsp' },
-        -- { name = 'vsnip' }, -- For vsnip users.
-        { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
-      }, {
+        { name = 'luasnip' },
+        { name = 'path' },
         { name = 'buffer' },
-      })
-    })
+      },
+      formatting = {
+        format = lspkind.cmp_format {
+          mode = 'symbol_text',
+        },
+      },
+    }
 
     -- Set configuration for specific filetype.
     cmp.setup.filetype('gitcommit', {
@@ -68,15 +74,15 @@ return {
         { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
       }, {
         { name = 'buffer' },
-      })
+      }),
     })
 
     -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline({ '/', '?' }, {
       mapping = cmp.mapping.preset.cmdline(),
       sources = {
-        { name = 'buffer' }
-      }
+        { name = 'buffer' },
+      },
     })
 
     -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
@@ -87,5 +93,5 @@ return {
         { name = 'cmdline' },
       },
     })
-  end
+  end,
 }
